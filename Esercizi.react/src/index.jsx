29aa4831e15edb28de3e34b42d8de75c703858b1
Francesco Damiata <damiatafrancesco@gmail.com>
2024-05-07 21:33:33 +0200

    fix

--- Esercizi.react/src/index.jsx
+++ Esercizi.react/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './css/index.css'
+import './css/index.css' /* index.css creato dentro folder css e Welcome.jsx modificato dandogli la classe*/
 
 const root = createRoot(document.getElementById('root'));
 
